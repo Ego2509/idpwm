897f17d8fbcc4d638af9a4f7de1f6edbbc8531f8
Ego2509 <egoego2509@gmail.com>
2021-01-25 13:43:04 -0500

    commit 1

---
+++ main.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+/*
+Web Manager of the Group of Primary Immunodeficiencies.
+Developed by the bioinformatics area.
+*/
+const path=require('path');
+const express=require('express');
+const http=require('http');
+const httplog=require('http-debug').http;
+
+const app=express();
+const server=http.createServer(app);
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public/html')));
+
+http.debug = 2;
+
+const PORT=process.env.PORT||80;//3000;//80;
+server.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
