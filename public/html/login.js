a32a93e155872712c780b5b0363143257833db05
Emmanuel Gomez Ospina <egoego2509@gmail.com>
2021-02-03 15:02:50 -0500

    fixing frontend request 3

--- public/html/login.js
+++ public/html/login.js
@@ -42,9 +42,9 @@ window.onload=()=>{
         let route="http://idp.udea.edu.co:80/"// !important -> url+"/" at the end (url+route)
         let params={
             method:'POST',
-            // headers:{
-            //     'Content-Type':'application/json'//x-www-form-urlencoded' //cuz its a form
-            // },
+            headers:{
+                'Content-Type':'application/json'//x-www-form-urlencoded' //cuz its a form
+            },
             // mode:'no-cors', //this solved the problem with cors when mozilla dind't let me access my own API
             body:JSON.stringify(data)
         }
