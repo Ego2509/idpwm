2806ba9b3c2844e80cbd9530c8c3a1db41e57fe9
Emmanuel Gomez Ospina <egoego2509@gmail.com>
2021-02-04 18:15:45 -0500

    frontend-server-backend working

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ Developed by the bioinformatics area.
 const path=require('path');
 const express=require('express');
 const http=require('http');
-const httplog=require('http-debug').http;
+const db=require('db.js')
+
+// const httplog=require('http-debug').http;
 
 const app=express();
 const server=http.createServer(app);
@@ -23,15 +25,26 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 const PORT=process.env.PORT||80;//3000;//80;
 server.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
 
-
 //Login System
 //more at: https://developer.mozilla.org/en-US/docs/Web/API/Request
-app.post('/',(req,res)=>{
+app.post('/',(req,res/*,next*/)=>{
     console.log('POST /')
     console.log(req)
     res.json(req.body)// is th same as res.json(req.body)
+    let [u,p]=[req.body.u,req.body.p]
     //working
-    // res.status("200").end()
+    //login:
+    //req(json) ->mysql(db col)
+    //u         ->USER
+    //p         ->PASW
+    q=`SELECT USER=${u} FROM USERS`
+    db.query(q,(err,ans,fields)=>{
+        if(err) throw(err);
+        console.log(`ans: ${ans}`);
+        console.log(`fields: ${fields}`);
+        console.log(`password match: ${p}`);
+    })
+
 })
 
 //this is a backup file
